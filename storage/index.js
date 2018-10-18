30867c0d6025b356449c90e466f10ed50651a49d
asyed94 <asyed94@gmail.com>
2018-10-18 16:49:40 -0500

    Replaces hand rolled sqlite3 promisifying with node-sqlite.

--- storage/index.js
+++ storage/index.js
@@ -1,49 +1,39 @@
-let path = require('path')
-let sqlite3 = require('sqlite3').verbose()
-let db = new sqlite3.Database(path.join(__dirname, '/db.sqlite3'))
+const path = require('path')
+const sqlite = require('sqlite')
 
-// Initialize the db with a 'storage' table
+let db = null
+let initialized = false
 
-let initTableQuery = `CREATE TABLE IF NOT EXISTS storage (
-  key TEXT PRIMARY KEY,
-  value BLOB
-)`
-
-db.serialize(function () {
-  db.run(initTableQuery)
-})
-
-// Helpers to promisify the sqlite3 bindings
-
-function promisify (fn, ...params) {
-  return new Promise((resolve, reject) => {
-    fn(...params, (err, resp) => {
-      if (err) reject(err)
-      if (resp) resolve(resp)
-      else resolve()
-    })
-  })
+/* Initilizes persistant storage.
+ * Returns a promise that is resolved when storage is ready.
+ */
+exports.init = async () => {
+  // Initialize the db with a 'storage' table
+  db = await sqlite.open(path.join(__dirname, '/db.sqlite3'))
+  let initTableQuery = `CREATE TABLE IF NOT EXISTS storage (
+    key TEXT PRIMARY KEY,
+    value BLOB
+  )`
+  await db.run(initTableQuery)
+  initialized = true
 }
-
-db.runPromise = (...params) => promisify(db.run.bind(db), ...params)
-db.getPromise = (...params) => promisify(db.get.bind(db), ...params)
-
-// Exposed storage interface
 
 /* Stores the given value with the given key into persistant storage.
  * Returns a promise that resolves when the store operation completes.
  */
 exports.set = (key, val) => {
+  if (!initialized) throw new Error('Storage not initialized.')
   let stringifiedVal = JSON.stringify(val)
   let setQuery = `INSERT OR REPLACE INTO storage (key, value) VALUES ('${key}', '${stringifiedVal}')`
-  return db.runPromise(setQuery)
+  return db.run(setQuery)
 }
 
 /* Gets the value referenced by the given key from persistant storage.
  * Returns a promise that resolves to the value.
  */
 exports.get = async (key) => {
+  if (!initialized) throw new Error('Storage not initialized.')
   let getQuery = `SELECT value FROM storage WHERE key IS '${key}'`
-  let result = await db.getPromise(getQuery)
+  let result = await db.get(getQuery)
   return JSON.parse(result.value)
 }
