589c8d92403457e8161117d3ee46c7049fd8d851
Aaron Sullivan <aasullivan1618@gmail.com>
2018-10-18 21:02:12 -0700

    Add test commands and proof specs

---
+++ test/unit/runner.spec.js
@@ -0,0 +1,9 @@
+// This is here only to test the test running setup and can be safely
+// removed once more tests are added.
+
+const test = require('tap').test
+
+test('runs the unit files', t => {
+  t.equal(true, true, 'runs the unit files')
+  t.end()
+})
