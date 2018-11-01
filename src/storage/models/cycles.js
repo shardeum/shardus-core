fc2034ea011621e769279f6f4fb9ad72bccad835
asyed94 <asyed94@gmail.com>
2018-11-01 15:14:08 -0500

    Makes counter primaryKey and unique in cycles model.

--- src/storage/models/cycles.js
+++ src/storage/models/cycles.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize')
 module.exports = [
   'cycles',
   {
-    counter: { type: Sequelize.BIGINT, allowNull: false },
+    counter: { type: Sequelize.BIGINT, unique: true, primaryKey: true, allowNull: false },
     marker: { type: Sequelize.TEXT, allowNull: false },
     active: { type: Sequelize.BIGINT, allowNull: false },
     desired: { type: Sequelize.BIGINT, allowNull: false },
