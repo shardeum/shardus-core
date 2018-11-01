d3738292aa592250e16210ed69f3a28c49c55bf7
asyed94 <asyed94@gmail.com>
2018-11-01 15:14:08 -0500

    Splits node.ip field into internalIp and externalIp.

--- src/storage/models/nodes.js
+++ src/storage/models/nodes.js
@@ -3,8 +3,9 @@ const Sequelize = require('sequelize')
 module.exports = [
   'nodes',
   {
-    id: { type: Sequelize.TEXT, allowNull: false, primaryKey: true},
-    ip: Sequelize.STRING,
+    id: { type: Sequelize.TEXT, allowNull: false, primaryKey: true },
+    internalIp: Sequelize.STRING,
+    externalIp: Sequelize.STRING,
     internalPort: Sequelize.SMALLINT,
     externalPort: Sequelize.SMALLINT,
     joinRequestTimestamp: Sequelize.BIGINT,
