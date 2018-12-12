f9fe93834f1782888e63c27ba4170655125a768a
asyed94 <asyed94@gmail.com>
2018-12-12 12:58:03 -0600

    Cleans up unit tests.

--- test/unit/runner.spec.js
+++
@@ -1,9 +0,0 @@
-// This is here only to test the test running setup and can be safely
-// removed once more tests are added.
-
-const test = require('tap').test
-
-test('runs the unit files', t => {
-  t.equal(true, true, 'runs the unit files')
-  t.end()
-})
