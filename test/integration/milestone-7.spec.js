87662b04d7ac544921a124be8a9361b5bfed4ac0
asyed94 <asyed94@gmail.com>
2018-12-13 12:13:18 -0600

    Changed tests to not use > 2 servers.

--- test/integration/milestone-7.spec.js
+++ test/integration/milestone-7.spec.js
@@ -1,4 +1,5 @@
 const { test, afterEach, tearDown } = require('tap')
+const axios = require('axios')
 const { sleep } = require('../../src/utils')
 const { isValidHex } = require('../includes/utils')
 const startUtils = require('../../tools/server-start-utils')({ baseDir: '../..' })
@@ -15,23 +16,23 @@ afterEach(async (t) => {
 
 test('Second node should compute and save its node_id and activate its internal API', async t => {
   await startUtils.startServer(seedNodePort, 9015)
-  const shardusSecond = await startUtils.startServerInstance(secondNodePort, 9016)
-  await sleep(cycleDuration * 1000)
+  await startUtils.startServer(secondNodePort, 9016, 'active')
 
-  const secondNodeId = await shardusSecond.storage.getProperty('id')
+  await sleep(2 * cycleDuration * 1000)
 
+  const { data } = await axios(`http://127.0.0.1:${secondNodePort}/nodeinfo`)
+  const secondNodeId = data.nodeInfo.id
   let requests = await startUtils.getRequests(seedNodePort)
   requests = requests.map(r => r.url)
-
   t.equal(isValidHex(secondNodeId), true, 'Should save valid node_id to database')
   t.equal(requests.includes('active'), true, 'Should receive internal API request from second node')
 })
 
 test('Second node should use its internal API to sync its node list and cycle chain with the network ', async t => {
   await startUtils.startServer(seedNodePort, 9015)
-  await startUtils.startServer(secondNodePort, 9016)
+  await startUtils.startServer(secondNodePort, 9016, 'active')
 
-  await sleep(1.5 * cycleDuration * 1000)
+  await sleep(2 * cycleDuration * 1000)
 
   let seedState = await startUtils.getState(seedNodePort)
   let secondState = await startUtils.getState(secondNodePort)
--- test/integration/milestone-8.spec.js
+++ test/integration/milestone-8.spec.js
@@ -1,9 +1,10 @@
 const { test, afterEach, tearDown } = require('tap')
 const axios = require('axios')
 const { sleep } = require('../../src/utils')
-const startUtils = require('../../tools/server-start-utils')({ baseDir: '../..' })
+const startUtils = require('../../tools/server-start-utils')({ baseDir: '../..', verbose: true })
 
 const seedNodePort = 9001
+const secondNodePort = 9002
 const cycleDuration = 5
 
 startUtils.setDefaultConfig({ server: { cycleDuration } })
@@ -13,30 +14,31 @@ afterEach(async (t) => {
 })
 
 test('Second node should send "active" message to all known nodes', async t => {
-  await startUtils.startServers(2, seedNodePort, 9015)
-  await startUtils.startServer(9003, 8003)
-  const { data } = await axios(`http://127.0.0.1:9003/nodeinfo`)
+  await startUtils.startServer(seedNodePort, 9015)
+  await startUtils.startServer(secondNodePort, 9016, 'active')
+
+  await sleep(2 * cycleDuration * 1000)
+
+  const { data } = await axios(`http://127.0.0.1:${secondNodePort}/nodeinfo`)
   const nodeId = data.nodeInfo.id
-  await sleep(cycleDuration * 1000)
-  for (let i = 0; i < 2; i += 1) {
-    const requests = await startUtils.getRequests(seedNodePort + i)
-    const activeMessages = requests.filter(r => r.url === 'active').map(r => r.body.nodeId)
-    t.equal(activeMessages.includes(nodeId), true, 'Should send active message to other nodes')
-  }
+  const requests = await startUtils.getRequests(seedNodePort)
+  const activeMessages = requests.filter(r => r.url === 'active').map(r => r.body.nodeId)
+  t.equal(activeMessages.includes(nodeId), true, 'Should send active message to other nodes')
 })
 
 test('Network should add nodes active state to the cycle chain', async t => {
-  await startUtils.startServers(2, seedNodePort, 9015)
-  await startUtils.startServer(9003, 8003)
-  await sleep(3 * cycleDuration * 2000)
+  await startUtils.startServer(seedNodePort, 9015)
+  await startUtils.startServer(secondNodePort, 9016, 'active')
+
+  await sleep(2 * cycleDuration * 1000)
+
   const { data } = await axios(`http://127.0.0.1:${seedNodePort}/cyclechain`)
   const cycleChain = data.cycleChain
   const lastCycle = cycleChain[cycleChain.length - 1]
   const { nodes } = await startUtils.getState(seedNodePort)
-  const lastJoinedNode = nodes.find(n => n.externalPort === 9003)
-  t.equal(lastCycle.active, 3, 'Seed node should have  3 active nodes in cycle chain')
+  const lastJoinedNode = nodes.find(n => n.externalPort === secondNodePort)
+  t.equal(lastCycle.active, 2, 'Seed node should have 2 active nodes in cycle chain')
   t.equal(lastJoinedNode.status, 'active', 'Network should update all node lists to mark node as active')
-  // TODO: check same things for second node at port 9002. Currently, second node is not adding new node into nodelist
 })
 
 tearDown(async () => {
