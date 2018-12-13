b180d7cc7fd6b03e9556ce24080f922328464382
asyed94 <asyed94@gmail.com>
2018-12-12 18:08:23 -0600

    Fixes ms-5 integration tests.

--- test/integration/milestone-5.spec.js
+++ test/integration/milestone-5.spec.js
@@ -13,7 +13,7 @@ afterEach(async (t) => {
 })
 
 test('seed node should have a `/join` endpoint on its external API', async t => {
-  await startUtils.startServer(9001, 9005)
+  await startUtils.startServer(9001, 9005, 'id')
   try {
     await axios.post(`http://127.0.0.1:${seedNodePort}/join`, {})
   } catch (e) {
@@ -23,14 +23,14 @@ test('seed node should have a `/join` endpoint on its external API', async t => 
 })
 
 test('second node should send a join request to the seed node `/join` endpoint', async t => {
-  await startUtils.startServers(2, seedNodePort, 9015)
+  await startUtils.startServers(2, seedNodePort, 9015, 'id')
   const requests = await startUtils.getRequests(seedNodePort)
   const joinRequest = requests.find(r => r.url === '/join' && r.method === 'POST')
   t.equal(joinRequest.body.nodeInfo.externalPort, secondNodePort, 'Seed node recieves join request from second node')
 })
 
 test('second node should poll the seed nodes `/cyclemarker` endpoint to check if it was accepted', async t => {
-  await startUtils.startServers(2, seedNodePort, 9015)
+  await startUtils.startServers(2, seedNodePort, 9015, 'id')
   const requests = await startUtils.getRequests(seedNodePort)
   let cycleMarkerRequests = requests.filter(r => r.url === '/cyclemarker')
   t.equal(cycleMarkerRequests.length > 1, true, 'Should seed node receive more than one cyclemarker requests from second node')
