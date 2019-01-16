b222ce93aac7d9ebca3962ea795d6da9e5af9a35
asyed94 <asyed94@gmail.com>
2019-01-16 13:05:30 -0600

    Adds 20 node integration test.

---
+++ test/integration/20-node-test.js
@@ -0,0 +1,14 @@
+const su = require('../../tools/server-start-utils')({
+  baseDir: '../..',
+  verbose: true,
+  nodeUpTimeout: 120000
+})
+
+async function main () {
+  for (let i = 1; i < 21; i++) {
+    await su.startServer(9000 + i, 10000 + i, 'active')
+    console.log()
+  }
+}
+
+main()
