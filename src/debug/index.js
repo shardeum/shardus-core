aae2e3c7f1acae31ec7d78e109bc007dc8c5a4a6
asyed94 <asyed94@gmail.com>
2019-02-06 13:25:36 -0500

    Changed debug endpoint to use nodes built in gzip compression instead of zip.

--- src/debug/index.js
+++ src/debug/index.js
@@ -1,14 +1,14 @@
 const fs = require('fs')
-const { join, resolve, parse } = require('path')
-const archiver = require('archiver')
+const path = require('path')
+const tar = require('tar')
 
 class Debug {
   constructor (baseDir, logger, storage, network) {
     this.network = network
-    this.archiveName = `debug-${network.ipInfo.externalIp}-${network.ipInfo.externalPort}.zip`
-    this.archiveFilePath = resolve(join(baseDir, this.archiveName))
+    this.archiveName = `debug-${network.ipInfo.externalIp}-${network.ipInfo.externalPort}.tar.gz`
+    this.archiveFilePath = path.join(baseDir, this.archiveName)
     this.logsDirPath = logger.logDir
-    this.dbDirPath = parse(storage.storage.storageConfig.options.storage).dir
+    this.dbDirPath = path.parse(storage.storage.storageConfig.options.storage).dir
   }
 
   async init () {
@@ -17,23 +17,66 @@ class Debug {
 
   _registerRoutes () {
     this.network.registerExternalGet('debug', async (req, res) => {
-      await this._createArchive({ output: this.archiveFilePath, logs: this.logsDirPath, db: this.dbDirPath })
+      await createArchive({ output: this.archiveFilePath, logs: this.logsDirPath, db: this.dbDirPath })
       res.download(this.archiveFilePath, this.archiveName)
-    })
-  }
-
-  _createArchive ({ output, logs, db }) {
-    return new Promise((resolve, reject) => {
-      const archive = archiver('zip', { zlib: { level: 9 } })
-      const stream = fs.createWriteStream(output)
-      stream.on('close', resolve)
-      archive.on('error', reject)
-      archive.pipe(stream)
-      archive.directory(logs, 'logs')
-      archive.directory(db, 'db')
-      archive.finalize()
     })
   }
 }
 
 module.exports = Debug
+
+async function createArchive ({ output, logs, db }) {
+  output = path.resolve(output)
+  logs = path.resolve(logs)
+  db = path.resolve(db)
+
+  const tmpDir = output.replace(/\.tar\.gz/, '')
+  const outputDir = tmpDir.split(path.sep).slice(0, -1).join(path.sep)
+
+  await copyDirSync(logs, path.join(tmpDir, 'logs'))
+  await copyDirSync(db, path.join(tmpDir, 'db'))
+
+  await tar.create({ gzip: true, file: output, cwd: outputDir }, [path.relative(outputDir, tmpDir)])
+
+  rimraf(tmpDir)
+}
+
+async function ensureExists (dir) {
+  return new Promise((resolve, reject) => {
+    fs.mkdir(dir, { recursive: true }, (err) => {
+      if (err) {
+        // Ignore err if folder exists
+        if (err.code === 'EEXIST') resolve()
+        // Something else went wrong
+        else reject(err)
+      } else {
+        // Successfully created folder
+        resolve()
+      }
+    })
+  })
+}
+
+async function copyDirSync (src, tgt) {
+  await ensureExists(tgt)
+  const files = fs.readdirSync(src)
+  for (const file of files) {
+    const srcFilePath = path.join(src, file)
+    const tgtFilePath = path.join(tgt, file)
+    fs.lstatSync(srcFilePath).isDirectory() ? await copyDirSync(srcFilePath, tgt) : fs.copyFileSync(srcFilePath, tgtFilePath)
+  }
+}
+
+function rimraf (dir) {
+  if (fs.existsSync(dir)) {
+    fs.readdirSync(dir).forEach(function (entry) {
+      var entryPath = path.join(dir, entry)
+      if (fs.lstatSync(entryPath).isDirectory()) {
+        rimraf(entryPath)
+      } else {
+        fs.unlinkSync(entryPath)
+      }
+    })
+    fs.rmdirSync(dir)
+  }
+}
--- test/integration/3-node-test-simultaneous.js
+++ test/integration/3-node-test-simultaneous.js
@@ -5,16 +5,10 @@ const su = require('../../tools/server-start-utils')({
 })
 
 async function main () {
-  await su.startServer(9001, 10001, 'active', null, false)
-  console.log()
-
-  await Promise.all([
-    su.startServer(9002, 10002, 'active', null, false),
-    su.startServer(9003, 10003, 'active', null, false)
-  ])
+  await su.startServers(3, 9001, 10001, 'active')
 }
 
 main()
-  .finally(() => {
-    su.stopAllServers()
-  })
+// .finally(() => {
+//   su.stopAllServers()
+// })
