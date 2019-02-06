2cd0c39401cbb738827aad4326a69acf29e7e4af
Derrick Farris <mr.dcfarris@gmail.com>
2019-02-06 16:29:05 -0600

    Add random module for seeded RNG

---
+++ src/random/index.js
@@ -0,0 +1,61 @@
+let crypto
+try {
+  crypto = require('crypto')
+} catch (err) {
+  console.log('Crypto support is disabled!')
+}
+
+const parseSeed = (seed) => {
+  if (typeof seed !== 'string' || seed.length !== 32) {
+    return false
+  }
+  const parsed = []
+  let processed = 0
+  for (let i = 0; i < 4; i++) {
+    const start = 0 + (processed * 8)
+    const end = start + 8
+    const hex = seed.slice(start, end)
+    parsed[i] = parseInt(hex, 16)
+    processed += 1
+  }
+  return parsed
+}
+
+// From StackOverflow: https://stackoverflow.com/a/47593316
+const sfc32 = (a, b, c, d) => {
+  return () => {
+    a >>>= 0; b >>>= 0; c >>>= 0; d >>>= 0
+    let t = (a + b) | 0
+    a = b ^ b >>> 9
+    b = c + (c << 3) | 0
+    c = (c << 21 | c >>> 11)
+    d = d + 1 | 0
+    t = t + d | 0
+    c = c + t | 0
+    return (t >>> 0) / 4294967296
+  }
+}
+
+const generateContext = (seed) => {
+  const generateSeed = () => {
+    const bytes = crypto.randomBytes(16)
+    return bytes.toString('hex')
+  }
+
+  if (!seed) {
+    seed = generateSeed()
+  }
+
+  const parsedSeed = parseSeed(seed)
+  if (!parsedSeed) return false
+
+  const rand = sfc32(parsedSeed[0], parsedSeed[1], parsedSeed[2], parsedSeed[3])
+
+  const randomInt = (min, max) => {
+    return Math.floor(rand() * (max - min + 1)) + min
+  }
+
+  return { generateSeed, rand, randomInt }
+}
+
+module.exports = generateContext
---
+++ src/random/random-test.js
@@ -0,0 +1,17 @@
+const Random = require('./index')
+
+const random = Random()
+
+console.log(`Result of 'random.rand()': ${random.rand()}`)
+console.log(`Result of 'random.randomInt(0, 1000)': ${random.randomInt(0, 1000)}\n`)
+
+const seed1 = random.generateSeed()
+const seed2 = random.generateSeed()
+
+const random1 = Random(seed1)
+const random2 = Random(seed1)
+const random3 = Random(seed2)
+console.log(`Result of 'random.randomInt(0, 1000)' for two instances of random with the same seed, and one instance with a different seed:`)
+console.log(`Instance 1: ${random1.randomInt(0, 1000)}`)
+console.log(`Instance 2: ${random2.randomInt(0, 1000)}`)
+console.log(`Instance 3 (different seed): ${random3.randomInt(0, 1000)}`)
