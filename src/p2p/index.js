9532b37b4e505f02a7263eaf79142067c425bc27
Derrick Farris <mr.dcfarris@gmail.com>
2019-04-01 15:44:21 -0500

    Fix checking for existence of an array to checking existence of the 0 index of that array

--- src/p2p/index.js
+++ src/p2p/index.js
@@ -643,7 +643,7 @@ class P2P extends EventEmitter {
       return { cycleJoined }
     }
     let query
-    while (!query) {
+    while (!query[0]) {
       try {
         query = await this.robustQuery(seedNodes, queryFn)
       } catch (e) {
