9e3963810e352d0027f590316a4c342d8678d903
Andrew Foster <3023784-afost@users.noreply.gitlab.com>
2019-05-22 18:45:05 -0500

    first pass on some shard value calculations

---
+++ test/unit/state-manager/shardvalues.js
@@ -0,0 +1,10 @@
+const StateManager = require('../../../src/state-manager')
+
+
+
+
+
+StateManager._updateShardValues(100, 30, '4098ffff' + '3'.repeat(52))
+
+
+StateManager._updateShardValues(20, 30, '0011ffff' + '3'.repeat(52))
