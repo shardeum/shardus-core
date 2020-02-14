9a4e88687def0cfe0992709d4220ee9a85a92810
Kyle <kyle@shardus.com>
2020-02-14 14:52:48 -0600

    Updated shardus configuration type with documentation

--- src/shardus/shardus-types.ts
+++ src/shardus/shardus-types.ts
@@ -347,59 +347,73 @@ declare namespace Shardus {
   enum Results { pass, fail }
 
   export interface ShardusConfiguration {
+    /** The heartbeatInterval parameter is an Integer that defines the number of seconds between each heartbeat logged within shardus */
     heartbeatInterval?: number,
+    /** The baseDir parameter is a String that defines the relative base directory for this running instance of shardus */
     baseDir?: string,
+    /** The transactionExpireTime parameter is an Integer that defines the amount of time (in seconds) allowed to pass before a transaction will expire and be rejected by the network. */
     transactionExpireTime?: number,
     /** Crypto module configuration */
     crypto?: {
-      /** The hash key for initializing the crypto module */
+      /** The hashkey parameter is a String that is used to initialize the crypto module, which is used for the cryptographic functions within shardus */
       hashKey?: string
     },
     /** P2P module configuration */
     p2p?: {
-      /**  */
+      /** The ipServer parameter is a String that specifies a the url for the ipServer. */
       ipServer?: string,
+      /** The timeServers parameter is an Array of String that specifies where to get time critical data. */
+      timeServers?: string[],
       /**  */
-      timeServer?: string,
-      /**  */
+      existingArchivers?: Array<{
+        ip: string
+        port: number
+        publicKey: string
+      }>
+      /** The syncLimit parameter is an Integer that specifies the amount of time (in seconds) a node’s local time can differ from the network’s time. */
       syncLimit?: number,
-      /**  */
+      /** The cycleDuration parameter is an Integer specifying the amount of time (in seconds) it takes for a shardus network cycle to complete. */
       cycleDuration?: number,
-      /**  */
+      /** The maxRejoinTime parameter is an Integer specifying the amount of time (in seconds) between network heartbeats before a node must ask to rejoin. */
       maxRejoinTime?: number,
-      /**  */
+      /** The seedList parameter is a String specifying the url for the seedNode server that the application will communicate with. */
       seedList?: string,
-      /**  */
+      /** The difficulty parameter is an Integer specifying the proof of work difficulty to prevent network spam. */
       difficulty?: number,
-      /**  */
+      /** The queryDelay parameter is an Integer specifying the amount of time (in seconds) to delay between cycle phase. */
       queryDelay?: number,
-      /**  */
+      /** The netadmin parameter is a String specifying the public key of the network admin for emergency network alerts, updates, or broadcasts. */
       netadmin?: string,
-      /**  */
+      /** The gossipRecipients parameter is an Integer specifying the number of nodes to send gossip to in the network after receiving a message. 
+       * Shardus groups nodes with neighbors, who they can gossip the message to, so you can set this pretty low and still expect it to be 
+       * propogated through the entire network. (It’s recommended to set this to AT LEAST 3, 4 is recommended, and 5 would be even safer, 
+       * but maybe overkill). Shardus will send 2 gossips to neighboring nodes, and send the remaining number left over in the parameter to 
+       * random nodes in the network, so messages will be propagated very quickly. 
+       **/
       gossipRecipients?: number,
-      /**  */
+      /** The gossipTimeout parameter is an Integer specifying the amount of time (in seconds) before an old gossip is deleted from a node. */
       gossipTimeout?: number,
-      
+      /** The maxSeedNodes parameter is an Integer specifying the maximum number of seedNodes used to be used. */
       maxSeedNodes?: number,
-      /**  */
+      /** The minNodes parameter is an Integer specifying the minimum number of nodes that need to be active in the network in order to process transactions. */
       minNodes?: number,
-      /**  */
+      /** The maxNodes parameter is an Integer specifying the maximum number of nodes that can be active in the network at once. */
       maxNodes?: number,
-      /**  */
+      /** The seedNodeOffset parameter is an Integer specifying the number of seedNodes to remove when producing the seedList */
       seedNodeOffset?: number,
-      /**  */
+      /** The nodeExpiryAge parameter is an Integer specifying the amount of time (in seconds) before a node can be in the network before getting rotated out. */
       nodeExpiryAge?: number,
-      /**  */
+      /** The maxNodesToRotate parameter is an Integer specifying the maximum number of nodes that can be rotated out of the network. */
       maxNodesToRotate?: number,
-      /**  */
+      /** The maxNodesPerCycle parameter is an Integer specifying the maximum number of nodes that can join the syncing phase each cycle. */
       maxNodesPerCycle?: number,
-      /**  */
+      /** The maxPercentOfDelta parameter is an Integer specifying the percent out of 100 that additional nodes can be accepted to the network. */
       maxPercentOfDelta?: number,
-      /**  */
+      /** The scaleReqsNeeded parameter is an Integer specyifying the number of internal scaling requests shardus needs to receive before scaling up or down the number of desired nodes in the network. */
       scaleReqsNeeded?: number,
-      /**  */
+      /** The maxScaleReqs parameter is an Integer specifying the maximum number of scaling requests the network will process before scaling up or down. */
       maxScaleReqs?: number,
-      /**  */
+      /** The amountToScale parameter is an Integer specifying the amount of nodes to add or remove from the number of desired nodes the network wants. */
       amountToScale?: number
     },
      /** Server IP configuration */
@@ -415,58 +429,73 @@ declare namespace Shardus {
     },
     /** Server Network module configuration */
     network?: {
-      /**  */
+      /** The timeout parameter is an Integer specifying the amount of time (in seconds) given to an internal network request made by the node until it gets timed out. */
       timeout?: number
     },
     /** Server Report module configuration */
     reporting?: {
-      /**  */
+      /** The report parameter is an Boolean specifying whether or not to report data to a monitor server / client. */
       report?: boolean,
-      /**  */
+      /** The recipient parameter is an String specifying the url of the recipient of the data that will be reported if report is set to true. */
       recipient?: string,
-      /**  */
+      /** The interval paramter is an Integer specifying the amount of time (in seconds) between the reported data updates. */
       interval?: number,
-      /**  */
+      /** The console parameter is an Boolean specifying whether or not to report data updates to the console. */
       console?: boolean
     },
     /** Server Debug module configuration */
     debug?: {
-      /**  */
+      /** The loseReceiptChance parameter is a Float specifying a percentage chance to randomly drop a receipt (currently doesn’t do anything) */
       loseReceiptChance?: number,
-      /**  */
+      /** The loseTxChance parameter is a Float specifying a percentage chance to randomly drop a transaction. */
       loseTxChance?: number
+      /** The canDataRepair parameter is a boolean that allows dataRepair to be turned on/off by the application (true = on | false = off) */
+      canDataRepair?: boolean
     },
-    /**  */
+    /** Options for the statistics module */
     statistics?: {
-      /**  */
+      /** The save parameter is a Boolean specifying whether or not statistics will be gathered and saved when running the network. */
       save?: boolean,
-      /**  */
+      /** The interval parameter is a Integer specifying the amount of time (in seconds) between each generated stats data. */
       interval?: number
-    },
+    }
     /**  */
     loadDetection?: {
-      /**  */
+      /** 
+       * The queueLimit parameter is an Integer which specifies one of the two possible limits to check whether the network is under heavy load. 
+       * It does this by checking it’s set value against the current transaction queue. The threshold will be equal to the number of transactions 
+       * in the queue / the queueLimit. 
+       **/
       queueLimit?: number,
-      /**  */
+      /** The desiredTxTime parameter is an Integer which specifies the other condition to check whether the network is under heavy load. */
       desiredTxTime?: number,
-      /**  */
+      /** The highThreshold parameter is an Integer which specifies the high end of the load the network can take. Reaching this threshold will cause the network to increase the desired nodes. */
       highThreshold?: number,
-      /**  */
+      /** The lowThreshold parameter is an Integer which specifies the low end of the load the network can take. Reaching this threshold will cause the network to decrease the desired nodes. */
       lowThreshold?: number
-    },
-    /**  */
+    }
+    /** Options for rate limiting */
     rateLimiting?: {
-      /**  */
+      /** The limitRate parameter is a Boolean indicating whether or not the network should rate limit in any way. */
       limitRate?: boolean,
-      /**  */
+      /** 
+       * The loadLimit parameter is a Float (between 0 and 1) indicating the maximum level of load the network can handle before starting to drop transactions. 
+       * With loadLimit set to 0.5, at 75% or 0.75 load, the network would drop 50% of incoming transactions. 
+       * (The percentage of chance to drop a transaction scales linearly as the load increases past the threshold). 
+       **/
       loadLimit?: number
-    },
+    }
     /** Server State manager module configuration */
     stateManager?: {
-      /**  */
+      /** The stateTableBucketSize parameter is an Integer which defines the max number of accountRecords that the p2p module will ask for in it’s get_account_state call. */
       stateTableBucketSize?: number,
-      /**  */
+      /** The accountBucketSize This is also currently used as input to a p2p ask method for the max number of account records */
       accountBucketSize?: number
+    }
+    /** Options for sharding calculations */
+    sharding?: {
+      /** The nodesPerConsensusGroup parameter defines how many nodes will be contained within a shard */
+      nodesPerConsensusGroup?: number
     }
   }
 
