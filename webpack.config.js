3117f6bb1a4d5f7839342065ab7dad0bb023bb46
Kyle <kyle@shardus.com>
2020-03-11 17:12:33 -0500

    Good version of the build script finally

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const nodeExternals = require('webpack-node-externals')
 const BytenodeWebpackPlugin = require('bytenode-webpack-plugin')
 
 module.exports = {
-  entry: './build/src/index.js',
+  entry: './build/src/',
   output: {
     filename: 'shardus.js',
     path: path.resolve(__dirname, process.env.npm_package_config_dist),
