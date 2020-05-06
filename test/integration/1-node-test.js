e5df1633f76d1664398a35f93238ca6eb0b5120d
Aamir Syed <asyed94@gmail.com>
2020-05-06 18:25:47 -0500

    Cleans up old/unused code and packages. Updates package versions to fix npm reported vulnerabilities

--- test/integration/1-node-test.js
+++
@@ -1,9 +0,0 @@
-const su = require('../../tools/server-start-utils')({
-  baseDir: '../..',
-  verbose: true
-})
-
-async function main () {
-  su.startServer(9001, 10001, null, { server: { reporting: { report: false } } }, false)
-}
-main()
--- test/integration/2-node-test.js
+++
@@ -1,9 +0,0 @@
-const su = require('../../tools/server-start-utils')({
-  baseDir: '../..',
-  verbose: true
-})
-
-async function main () {
-  su.startServers(2, null, null, 'id', null, false)
-}
-main()
