5a9de97482e5f77088bfc2be68204d6b33a64858
Aamir Syed <asyed94@gmail.com>
2020-05-15 19:18:52 -0500

    Improves build scripts for dist version

---
+++ scripts/build-compile.js
@@ -0,0 +1,55 @@
+const fs = require('fs')
+const path = require('path')
+
+const rootDir = path.resolve(__dirname, '../build')
+const distDir = path.resolve(rootDir, '../', process.env.npm_package_config_dist)
+
+// Load package.json, modify for distribution, and write into dist/
+const packageJson = JSON.parse(fs.readFileSync(path.join(__dirname, '../package.json'), { encoding: 'utf8' }))
+
+const packageJsonNew = Object.assign({}, packageJson)
+packageJsonNew.name = packageJson.name + '-dist'
+packageJsonNew.description = `Compiled version of ${packageJson.name}.`
+packageJsonNew.main = './index.js'
+delete packageJsonNew.types
+delete packageJsonNew.files
+delete packageJsonNew.config
+delete packageJsonNew.scripts
+delete packageJsonNew.repository
+delete packageJsonNew.devDependencies
+
+const packageLockJson = JSON.parse(fs.readFileSync(path.join(__dirname, '../package-lock.json'), { encoding: 'utf8' }))
+packageJsonNew.dependencies['bytenode'] = packageLockJson.dependencies.bytenode.version
+
+fs.writeFileSync(path.join(distDir, 'package.json'), JSON.stringify(packageJsonNew, null, 2), { flag: 'w' })
+
+// Copy config/, into dist/
+copyFolderSync(path.join(rootDir, 'src/config'), path.join(distDir, 'config'))
+
+// Copy src/.../computePowGenerator.js and scripts/build-index.js into dist/
+fs.copyFileSync(path.join(rootDir, 'src/crypto/computePowGenerator.js'), path.join(distDir, 'computePowGenerator.js'))
+fs.copyFileSync(path.join(__dirname, '../', 'scripts/build-index.js'), path.join(distDir, 'index.js'))
+
+// Copy build/src/shardus/shardus-types.d.ts into dist/src/shardus/shardus-types.d.ts
+fs.mkdirSync(path.join(distDir, 'src'), { recursive: true })
+fs.mkdirSync(path.join(distDir, 'src/shardus'), { recursive: true })
+fs.copyFileSync(path.join(rootDir, 'src/shardus/shardus-types.d.ts'), path.join(distDir, 'src/shardus/shardus-types.d.ts'))
+
+// Modified from https://stackoverflow.com/a/52338335
+function copyFolderSync(from, to) {
+  try {
+    fs.mkdirSync(to, {recursive: true})
+  } catch (e) {
+    if (e.code !== 'EEXIST') {
+      console.log(e)
+      return
+    }
+  }
+  fs.readdirSync(from).forEach(element => {
+    if (fs.lstatSync(path.join(from, element)).isFile()) {
+      fs.copyFileSync(path.join(from, element), path.join(to, element))
+    } else {
+      copyFolderSync(path.join(from, element), path.join(to, element))
+    }
+  })
+}
--- scripts/update-dist.js
+++ scripts/update-dist.js
@@ -11,18 +11,8 @@ if (shardusCommit === reflectedDistCommit) {
   process.exit()
 }
 
-// Delete ./dist
-rm('-rf', './dist')
-console.log("Deleted './dist'")
-
-// git clone https://gitlab.com/shardus/shardus-globla-server-dist.git ./dist
-execa.commandSync('git clone https://gitlab.com/shardus/global/shardus-global-server-dist.git ./dist', { stdio: [0, 1, 2] })
-
 // npm run build
 execa.commandSync('npm run build', { stdio: [0, 1, 2] })
-
-// cwd: ./dist; npm i
-execa.commandSync('npm i', { cwd: './dist', stdio: [0, 1, 2] })
 
 // cwd: ./dist; git add .
 execa.commandSync('git add .', { cwd: './dist', stdio: [0, 1, 2] })
