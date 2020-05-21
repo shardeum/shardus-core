9b917d4b189ace6001f1d731baa1c225e33145b1
Aamir Syed <asyed94@gmail.com>
2020-05-20 19:35:21 -0500

    Updated update-dist-arm-from-x86 script"

--- scripts/update-dist-arm-from-x86.sh
+++ scripts/update-dist-arm-from-x86.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 docker run --platform linux/arm/v7 -it --rm -w /usr/src/app -v $(pwd):/usr/src/app --entrypoint /usr/local/bin/npm node:12 run update-dist-arm
+sudo chown -R $(whoami):$(whoami) dist/
