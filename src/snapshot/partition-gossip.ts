0821cc3ab5f751f9cee4095975ddcad4fd48e880
Thant Sin Toe <mr.thantsintoe@gmail.com>
2020-06-25 20:13:40 +0630

    gossip the partition hash, count hash tally and store to DB

--- src/snapshot/partition-gossip.ts
+++ src/snapshot/partition-gossip.ts
@@ -41,32 +41,57 @@ export class PartitionGossip extends EventEmitter {
     // Register gossip handler for this cycles partition hash gossip
     this.handler = 'snapshot' + this.shard.cycleNumber
     registerGossipHandler(this.handler, (payload) => {
-      const partitionHashes = new Map(payload) as PartitionHashes
+      const partitionHashes = new Map() as PartitionHashes
+      for (let partitionId in payload) {
+        partitionHashes.set(parseInt(partitionId), payload[partitionId])
+      }
 
-      // [TODO] Check nodeToPartitions to make sure all PartitionIds in payload belong to sender
-      // [NOTE] Implement this last after everything is working
+      // // [TODO] Check nodeToPartitions to make sure all PartitionIds in payload belong to sender
+      // // [NOTE] Implement this last after everything is working
 
-      // [TODO] Add partition hashes into hashTally
+      // Add partition hashes into hashTally
+      for (let [partitionId, hash] of partitionHashes) {
+        if (!this.hashCounter.has(partitionId)) {
+          this.hashCounter.set(partitionId, new Map([[hash, 1]]))
+        } else if (this.hashCounter.has(partitionId)) {
+          let counterMap = this.hashCounter.get(partitionId)
+          counterMap.set(hash, counterMap.get(hash) + 1)
+        }
+      }
 
-      /**
-       * [TODO]
-       * Once hashTally has shard.shardGlobals.numPartitions number of PartitionId's,
-       *   For each PartitionId, take the hash with the highest count and put it into allHashes
-       * 
-       * // Emit an event once allHashes are collected
-       * this.emit('gotAllHashes', this.allHashes)
-       * 
-       * // Clean up the gossip handler for this cycles partition hash gossip
-       * unregisterGossipHandler(this.handler)
-       */
+      // Once hashTally count is same as shard.shardGlobals.numPartitions number of PartitionId's. 
+      // For each PartitionId, take the hash with the highest count and put it into allHashes
+      if (this.hashCounter.size === this.shard.shardGlobals.numPartitions) {
+        console.log('DEBUG', 'SNAPSHOT', `HashCounter size is same as numPartitions`)
+        for (let [partitionId, counterMap] of this.hashCounter) {
+          let selectedHash
+          let maxCount = 0
+          for (let [hash, counter] of counterMap) {
+            if (counter > maxCount) {
+              selectedHash = hash
+              maxCount = counter
+            }
+          }
+          this.allHashes.set(partitionId, selectedHash)
+        }
+        // Emit an event once allHashes are collected
+        this.emit('gotAllHashes', this.allHashes)
+
+        // Clean up the gossip handler for this cycles partition hash gossip
+        unregisterGossipHandler(this.handler)
+      }
 
       // Forward the gossip
-      sendGossip(this.handler, payload)
+      // sendGossip(this.handler, payload)
     })
   }
 
   send(hashes: PartitionHashes) {
-    sendGossip(this.handler, [...hashes])
+    let data = {}
+    for (let [partitionId, hash] of hashes) {
+      data[partitionId] = hash
+    }
+    sendGossip(this.handler, data)
   }
 }
 
