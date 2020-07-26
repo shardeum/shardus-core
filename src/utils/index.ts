8d668efdd296ffb36f0fd324e1eeecd40c55abb0
Aamir Syed <asyed94@gmail.com>
2020-07-25 23:27:37 -0500

    Fixes bug in utils.isObject causing arrays to be treated as objects

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -241,6 +241,9 @@ export const isObject = val => {
   if (val === null) {
     return false
   }
+  if (Array.isArray(val)) {
+    return false
+  }
   return typeof val === 'function' || typeof val === 'object'
 }
 
