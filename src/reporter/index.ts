65d90dff75f235360ada78dc58324ee524a4f25f
Thant Sin Toe <mr.thantsintoe@gmail.com>
2021-01-27 19:05:56 +0630

    collect tx statistics between each reporting to prvent wrong statistics when report inter is longer than statistics interval

--- src/reporter/index.ts
+++ src/reporter/index.ts
@@ -3,6 +3,7 @@ import LoadDetection from '../load-detection'
 import Logger from '../logger'
 import { ipInfo } from '../network'
 import { config, crypto } from '../p2p/Context'
+import * as Context from '../p2p/Context'
 import { getDesiredCount } from '../p2p/CycleAutoScale'
 import * as CycleChain from '../p2p/CycleChain'
 import * as NodeList from '../p2p/NodeList'
@@ -22,6 +23,14 @@ const allZeroes64 = '0'.repeat(64)
  * @typedef {import('../state-manager/index').CycleShardData} CycleShardData
  */
 
+interface StatisticsReport {
+  txInjected: number
+  txApplied: number
+  txRejected: number
+  txProcessed: number
+  txExpired: number
+}
+
 interface Reporter {
   config: any
   mainLogger: Log4js.Logger
@@ -35,10 +44,10 @@ interface Reporter {
   lastTime: number
   doConsoleReport: boolean
   hasRecipient: boolean
+  statisticsReport: StatisticsReport
 }
-
 class Reporter {
-  constructor(
+  constructor (
     config,
     logger,
     statistics,
@@ -63,13 +72,38 @@ class Reporter {
       this.doConsoleReport = true
     }
     this.hasRecipient = this.config.recipient != null
+    this.resetStatisticsReport()
   }
 
-  _calculateAverageTps(txs) {
-    return Math.round(txs / this.getReportInterval())
+  resetStatisticsReport () {
+    this.statisticsReport = {
+      txInjected: 0,
+      txApplied: 0,
+      txRejected: 0,
+      txProcessed: 0,
+      txExpired: 0,
+    }
   }
 
-  async reportJoining(publicKey) {
+  collectStatisticToReport () {
+    this.statisticsReport.txInjected += this.statistics
+      ? this.statistics.getPreviousElement('txInjected')
+      : 0
+    this.statisticsReport.txApplied += this.statistics
+      ? this.statistics.getPreviousElement('txApplied')
+      : 0
+    this.statisticsReport.txRejected += this.statistics
+      ? this.statistics.getPreviousElement('txRejected')
+      : 0
+    this.statisticsReport.txExpired += this.statistics
+      ? this.statistics.getPreviousElement('txExpired')
+      : 0
+    this.statisticsReport.txProcessed += this.statistics
+      ? this.statistics.getPreviousElement('txProcessed')
+      : 0
+  }
+
+  async reportJoining (publicKey) {
     if (!this.hasRecipient) {
       return
     }
@@ -87,7 +121,7 @@ class Reporter {
     }
   }
 
-  async reportJoined(nodeId, publicKey) {
+  async reportJoined (nodeId, publicKey) {
     if (!this.hasRecipient) {
       return
     }
@@ -106,7 +140,7 @@ class Reporter {
     }
   }
 
-  async reportActive(nodeId) {
+  async reportActive (nodeId) {
     if (!this.hasRecipient) {
       return
     }
@@ -120,7 +154,7 @@ class Reporter {
     }
   }
 
-  async reportRemoved(nodeId) {
+  async reportRemoved (nodeId) {
     if (!this.hasRecipient) {
       return
     }
@@ -138,7 +172,7 @@ class Reporter {
   }
 
   // Sends a report
-  async _sendReport(data) {
+  async _sendReport (data) {
     if (!this.hasRecipient) {
       return
     }
@@ -158,11 +192,10 @@ class Reporter {
     }
   }
 
-  getReportInterval(): number {
-    if (NodeList.activeByIdOrder.length >= 100) {
+  getReportInterval (): number {
+    if (NodeList.activeByIdOrder.length >= 5) {
       return 10 * 1000
     } else {
-      console.log('this.config', this.config)
       return this.config.interval * 1000
     }
   }
@@ -174,108 +207,107 @@ class Reporter {
         ? await this.stateManager.getAccountsStateHash()
         : allZeroes64
     */
-      let appState = allZeroes64 // monititor server will set color based on partition report
-      const cycleMarker = CycleChain.newest.previous || '' // [TODO] Replace with cycle creator
-      const cycleCounter = CycleChain.newest.counter
-      const nodelistHash = crypto.hash(NodeList.byJoinOrder)
-      const desiredNodes = getDesiredCount()
-      const txInjected = this.statistics
-        ? this.statistics.getPreviousElement('txInjected')
-        : 0
-      const txApplied = this.statistics
-        ? this.statistics.getPreviousElement('txApplied')
-        : 0
-      const txRejected = this.statistics
-        ? this.statistics.getPreviousElement('txRejected')
-        : 0
-      const txExpired = this.statistics
-        ? this.statistics.getPreviousElement('txExpired')
-        : 0
-      const txProcessed = this.statistics
-        ? this.statistics.getPreviousElement('txProcessed')
-        : 0
-      const reportInterval = this.getReportInterval()
-      const nodeIpInfo = ipInfo
+    let appState = allZeroes64 // monititor server will set color based on partition report
+    const cycleMarker = CycleChain.newest.previous || '' // [TODO] Replace with cycle creator
+    const cycleCounter = CycleChain.newest.counter
+    const nodelistHash = crypto.hash(NodeList.byJoinOrder)
+    const desiredNodes = getDesiredCount()
+    const txInjected = this.statisticsReport.txInjected
+    const txApplied = this.statisticsReport.txApplied
+    const txRejected = this.statisticsReport.txRejected
+    const txExpired = this.statisticsReport.txExpired
+    const txProcessed = this.statisticsReport.txProcessed
+    const reportInterval = this.getReportInterval()
+    const nodeIpInfo = ipInfo
 
-      let repairsStarted = 0
-      let repairsFinished = 0
-      // report only if we are active in te networks.
-      // only knowingly report deltas.
-      let partitionReport = null
-      let globalSync = null
-      if (this.stateManager != null) {
-        partitionReport = this.stateManager.getPartitionReport(true, true)
-        globalSync = this.stateManager.isStateGood()
+    let repairsStarted = 0
+    let repairsFinished = 0
+    // report only if we are active in te networks.
+    // only knowingly report deltas.
+    let partitionReport = null
+    let globalSync = null
+    if (this.stateManager != null) {
+      partitionReport = this.stateManager.getPartitionReport(true, true)
+      globalSync = this.stateManager.isStateGood()
 
-        repairsStarted = this.stateManager.dataRepairsStarted
-        repairsFinished = this.stateManager.dataRepairsCompleted
-        // Hack to code a green or red color for app state:
-        appState = globalSync ? '00ff00ff' : 'ff0000ff'
-      }
-
-      let partitions = 0
-      let partitionsCovered = 0
-      if (this.stateManager != null) {
-        /** @type {CycleShardData} */
-        const shardData = this.stateManager.currentCycleShardData //   getShardDataForCycle(cycleCounter)
-        if (shardData != null) {
-          partitions = shardData.shardGlobals.numPartitions
-          partitionsCovered =
-            shardData.nodeShardData.storedPartitions.partitionsCovered
-        }
-      }
-
-      // Server load
-      const currentLoad = this.loadDetection.getCurrentLoad()
-      const queueLength = this.statistics.getPreviousElement('queueLength')
-      const txTimeInQueue =
-        this.statistics.getPreviousElement('txTimeInQueue') / 1000 // ms to sec
-
-      try {
-        await this._sendReport({
-          repairsStarted,
-          repairsFinished,
-          appState,
-          cycleMarker,
-          cycleCounter,
-          nodelistHash,
-          desiredNodes,
-          txInjected,
-          txApplied,
-          txRejected,
-          txExpired,
-          txProcessed,
-          reportInterval,
-          nodeIpInfo,
-          partitionReport,
-          globalSync,
-          partitions,
-          partitionsCovered,
-          currentLoad,
-          queueLength,
-          txTimeInQueue,
-          shardusVersion: packageJson.version
-        })
-      } catch (e) {
-        this.mainLogger.error(
-          'startReporting: ' + e.name + ': ' + e.message + ' at ' + e.stack
-        )
-        console.error(e)
-      }
-
-      if (this.doConsoleReport) {
-        this.consoleReport()
-      }
-      this.reportTimer = setTimeout(() => { this.report() }, this.getReportInterval())
+      repairsStarted = this.stateManager.dataRepairsStarted
+      repairsFinished = this.stateManager.dataRepairsCompleted
+      // Hack to code a green or red color for app state:
+      appState = globalSync ? '00ff00ff' : 'ff0000ff'
     }
 
-  startReporting() {
-    let self = this
-    // Creates and sends a report every `interval` seconds
-    this.reportTimer = setTimeout(() => { this.report() }, this.getReportInterval())
+    let partitions = 0
+    let partitionsCovered = 0
+    if (this.stateManager != null) {
+      /** @type {CycleShardData} */
+      const shardData = this.stateManager.currentCycleShardData //   getShardDataForCycle(cycleCounter)
+      if (shardData != null) {
+        partitions = shardData.shardGlobals.numPartitions
+        partitionsCovered =
+          shardData.nodeShardData.storedPartitions.partitionsCovered
+      }
+    }
+
+    // Server load
+    const currentLoad = this.loadDetection.getCurrentLoad()
+    const queueLength = this.statistics.getPreviousElement('queueLength')
+    const txTimeInQueue =
+      this.statistics.getPreviousElement('txTimeInQueue') / 1000 // ms to sec
+
+    try {
+      await this._sendReport({
+        repairsStarted,
+        repairsFinished,
+        appState,
+        cycleMarker,
+        cycleCounter,
+        nodelistHash,
+        desiredNodes,
+        txInjected,
+        txApplied,
+        txRejected,
+        txExpired,
+        txProcessed,
+        reportInterval,
+        nodeIpInfo,
+        partitionReport,
+        globalSync,
+        partitions,
+        partitionsCovered,
+        currentLoad,
+        queueLength,
+        txTimeInQueue,
+        shardusVersion: packageJson.version,
+      })
+    } catch (e) {
+      this.mainLogger.error(
+        'startReporting: ' + e.name + ': ' + e.message + ' at ' + e.stack
+      )
+      console.error(e)
+    }
+
+    this.resetStatisticsReport()
+
+    if (this.doConsoleReport) {
+      this.consoleReport()
+    }
+    this.reportTimer = setTimeout(() => {
+      this.report()
+    }, this.getReportInterval())
   }
 
-  consoleReport() {
+  startReporting () {
+    let self = this
+    setInterval(() => {
+      self.collectStatisticToReport()
+    }, 1000)
+    // Creates and sends a report every `interval` seconds
+    this.reportTimer = setTimeout(() => {
+      this.report()
+    }, this.getReportInterval())
+  }
+
+  consoleReport () {
     const time = Date.now()
     let delta = time - this.lastTime
     delta = delta * 0.001
@@ -297,7 +329,7 @@ class Reporter {
     }
   }
 
-  stopReporting() {
+  stopReporting () {
     this.mainLogger.info('Stopping statistics reporting...')
     clearTimeout(this.reportTimer)
   }
