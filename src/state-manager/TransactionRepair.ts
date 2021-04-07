80a7579e52247aed3bf88404104263b1c423e15b
Andrew Foster <3023784-afost@users.noreply.gitlab.com>
2021-04-07 09:27:10 -0500

    More profiler sections and counters for transaction repair.

--- src/state-manager/TransactionRepair.ts
+++ src/state-manager/TransactionRepair.ts
@@ -13,6 +13,7 @@ import ShardFunctions from './shardFunctions.js'
 import { time } from 'console'
 import StateManager from '.'
 import { json } from 'sequelize/types'
+import { nestedCountersInstance } from '../utils/nestedCounters'
 
 class TransactionRepair {
   app: Shardus.App
@@ -64,6 +65,8 @@ class TransactionRepair {
       this.stateManager.dataRepairsStarted++
 
       this.profiler.profileSectionStart('repair')
+      this.profiler.profileSectionStart('repair_init')
+      nestedCountersInstance.countEvent('repair1', 'init')
 
       let shortHash = utils.makeShortHash(queueEntry.acceptedTx.id)
       // Need to build a list of what accounts we need, what state they should be in and who to get them from
@@ -86,6 +89,8 @@ class TransactionRepair {
       let upToDateAccounts: { [id: string]: boolean }  = {}
 
       let numUpToDateAccounts = 0
+
+      this.profiler.profileSectionEnd('repair_init')
 
       for (let key of queueEntry.uniqueKeys) {
         let coveredKey = false
@@ -259,121 +264,137 @@ class TransactionRepair {
                 }
               }
 
-              let message = { key: requestObject.accountId, hash: requestObject.accountHash, txid: queueEntry.acceptedTx.id, timestamp: queueEntry.acceptedTx.timestamp }
-              let result: RequestStateForTxResp = await this.p2p.ask(node, 'request_state_for_tx_post', message) // not sure if we should await this.
+              let result: RequestStateForTxResp
+              try{
+                this.profiler.profileSectionStart('repair_asking_for_data')
+                nestedCountersInstance.countEvent('repair1', 'asking')
+              
+                let message = { key: requestObject.accountId, hash: requestObject.accountHash, txid: queueEntry.acceptedTx.id, timestamp: queueEntry.acceptedTx.timestamp }
+                let result: RequestStateForTxResp = await this.p2p.ask(node, 'request_state_for_tx_post', message) // not sure if we should await this.
 
-              if (result == null) {
-                if (logFlags.verbose) {
-                  this.mainLogger.error(`ASK FAIL repairToMatchReceipt request_state_for_tx_post result == null tx:${shortHash}  acc:${shortKey}`)
+                if (result == null) {
+                  if (logFlags.verbose) {
+                    this.mainLogger.error(`ASK FAIL repairToMatchReceipt request_state_for_tx_post result == null tx:${shortHash}  acc:${shortKey}`)
+                  }
+                  // We shuffle the array of votes each time so hopefully will ask another node next time
+                  // TODO more robust limits to this process, maybe a delay?
+                  this.mainLogger.error(`ASK FAIL repairToMatchReceipt request_state_for_tx_post no reponse from ${utils.stringifyReduce(node.id)}  tx:${shortHash}  acc:${shortKey}`)
+                  //this.fatalLogger.fatal(`ASK FAIL repairToMatchReceipt missing logic to ask a new node! 1 ${utils.stringifyReduce(node.id)}`)
+                  node = null
+                  continue
                 }
-                // We shuffle the array of votes each time so hopefully will ask another node next time
-                // TODO more robust limits to this process, maybe a delay?
-                this.mainLogger.error(`ASK FAIL repairToMatchReceipt request_state_for_tx_post no reponse from ${utils.stringifyReduce(node.id)}  tx:${shortHash}  acc:${shortKey}`)
-                //this.fatalLogger.fatal(`ASK FAIL repairToMatchReceipt missing logic to ask a new node! 1 ${utils.stringifyReduce(node.id)}`)
-                node = null
-                continue
-              }
 
-              if (result.success !== true) {
-                this.mainLogger.error(`ASK FAIL repairToMatchReceipt result.success === ${result.success}   tx:${shortHash}  acc:${shortKey}`)
-                //this.fatalLogger.fatal(`ASK FAIL repairToMatchReceipt missing logic to ask a new node! 2 ${utils.stringifyReduce(node.id)}`)
-                node = null
-                continue
+                if (result.success !== true) {
+                  this.mainLogger.error(`ASK FAIL repairToMatchReceipt result.success === ${result.success}   tx:${shortHash}  acc:${shortKey}`)
+                  //this.fatalLogger.fatal(`ASK FAIL repairToMatchReceipt missing logic to ask a new node! 2 ${utils.stringifyReduce(node.id)}`)
+                  node = null
+                  continue
+                }
+              } finally{
+                this.profiler.profileSectionEnd('repair_asking_for_data')
               }
-
               let dataCountReturned = 0
               let accountIdsReturned = []
               for (let data of result.stateList) {
+                try{
+                  this.profiler.profileSectionStart('repair_saving_account_data')
+                  nestedCountersInstance.countEvent('repair1', 'saving')
+                  // let shortKey = utils.stringifyReduce(data.accountId)
 
-                // let shortKey = utils.stringifyReduce(data.accountId)
+                  if (logFlags.playback) this.logger.playbackLogNote('shrd_repairToMatchReceipt_note', `${shortHash}`, `write data: ${utils.stringifyReduce(data)}  acc:${shortKey}`)
+                  //Commit the data
+                  let dataToSet = [data]
+                  let failedHashes = await this.stateManager.checkAndSetAccountData(dataToSet, `tx:${shortHash} repairToMatchReceipt`, false)
+                  nestedCountersInstance.countEvent('repair1', 'writeCombinedAccountDataToBackups')
+                  await this.stateManager.writeCombinedAccountDataToBackups(dataToSet, failedHashes)
+                  attemptsRemaining = false
+                  //update global cache?  that will be obsolete soona anyhow!
+                  //need to loop and call update
+                  let beforeData = data.prevDataCopy
 
-                if (logFlags.playback) this.logger.playbackLogNote('shrd_repairToMatchReceipt_note', `${shortHash}`, `write data: ${utils.stringifyReduce(data)}  acc:${shortKey}`)
-                //Commit the data
-                let dataToSet = [data]
-                let failedHashes = await this.stateManager.checkAndSetAccountData(dataToSet, `tx:${shortHash} repairToMatchReceipt`, false)
-                await this.stateManager.writeCombinedAccountDataToBackups(dataToSet, failedHashes)
-                attemptsRemaining = false
-                //update global cache?  that will be obsolete soona anyhow!
-                //need to loop and call update
-                let beforeData = data.prevDataCopy
-
-                if (beforeData == null) {
-                  //prevDataCopy
-                  let wrappedAccountDataBefore = queueEntry.collectedData[data.accountId]
-                  if (wrappedAccountDataBefore != null) {
-                    beforeData = wrappedAccountDataBefore.data
-                  }
-                }
-                if (beforeData == null) {
-                  let results = await this.app.getAccountDataByList([data.accountId])
-                  beforeData = results[0]
-                  this.mainLogger.error(`repairToMatchReceipt: statsDataSummaryUpdate2 beforeData: had to query for data 1 ${utils.stringifyReduce(data.accountId)}  tx:${shortHash}  acc:${shortKey}`)
                   if (beforeData == null) {
-                    this.mainLogger.error(`repairToMatchReceipt: statsDataSummaryUpdate2 beforeData: had to query for data 1 not found ${utils.stringifyReduce(data.accountId)}  tx:${shortHash}  acc:${shortKey}`)
-                  }
-                }
-                if (beforeData == null) {
-                  this.mainLogger.error(`repairToMatchReceipt: statsDataSummaryUpdate2 beforeData data is null ${utils.stringifyReduce(data.accountId)} WILL CAUSE DATA OOS  tx:${shortHash}  acc:${shortKey}`)
-                } else {
-                  if (this.stateManager.partitionStats.hasAccountBeenSeenByStats(data.accountId) === false) {
-                    // Init stats because we have not seen this account yet.
-                    this.stateManager.partitionStats.statsDataSummaryInitRaw(queueEntry.cycleToRecordOn, data.accountId, beforeData)
-                  }
-
-                  // important to update the timestamp.  There are various reasons it could be incorrectly set to 0
-                  let { timestamp: updatedTimestamp, hash: updatedHash } = this.app.getTimestampAndHashFromAccount(data.data)
-                  if (data.timestamp != updatedTimestamp) {
-                    this.mainLogger.error(`repairToMatchReceipt: statsDataSummaryUpdate2 timstamp had to be corrected from ${data.timestamp} to ${updatedTimestamp}   tx:${shortHash}  acc:${shortKey} hash:${utils.stringifyReduce(updatedHash)} `)
-                  }
-                  // This is correcting the timestamp on the wrapper
-                  data.timestamp = updatedTimestamp
-
-                  // update stats
-                  this.stateManager.partitionStats.statsDataSummaryUpdate2(queueEntry.cycleToRecordOn, beforeData, data)
-
-                  // record state table data
-                  let { timestamp: oldtimestamp, hash: oldhash } = this.app.getTimestampAndHashFromAccount(beforeData)
-
-                  let hashNeededUpdate = oldhash !== result.beforeHashes[data.accountId]
-                  oldhash = result.beforeHashes[data.accountId]
-
-                  let stateTableResults: Shardus.StateTableObject = {
-                    accountId: data.accountId,
-                    txId: queueEntry.acceptedTx.id,
-                    stateBefore: oldhash,
-                    stateAfter: updatedHash,
-                    txTimestamp: `${updatedTimestamp}`,
-                  }
-
-                  let updateStateTable = true
-                  let timeStampMatches = updatedTimestamp === queueEntry.acceptedTx.timestamp
-                  let test2 = false
-                  if (timeStampMatches === false) {
-                    if (this.stateManager.accountGlobals.isGlobalAccount(data.accountId)) {
-                      updateStateTable = false
-                      test2 = true
+                    //prevDataCopy
+                    let wrappedAccountDataBefore = queueEntry.collectedData[data.accountId]
+                    if (wrappedAccountDataBefore != null) {
+                      beforeData = wrappedAccountDataBefore.data
                     }
                   }
-                  
-                  let isGlobal = this.stateManager.accountGlobals.isGlobalAccount(data.accountId)
-                  let test3 = false
-                  if (isGlobal) {
-                    if (oldhash === updatedHash) {
-                      updateStateTable = false
-                      test3 = true
+                  if (beforeData == null) {
+                    nestedCountersInstance.countEvent('repair1', 'getAccountDataByList')
+                    let results = await this.app.getAccountDataByList([data.accountId])
+                    beforeData = results[0]
+                    this.mainLogger.error(`repairToMatchReceipt: statsDataSummaryUpdate2 beforeData: had to query for data 1 ${utils.stringifyReduce(data.accountId)}  tx:${shortHash}  acc:${shortKey}`)
+                    if (beforeData == null) {
+                      this.mainLogger.error(`repairToMatchReceipt: statsDataSummaryUpdate2 beforeData: had to query for data 1 not found ${utils.stringifyReduce(data.accountId)}  tx:${shortHash}  acc:${shortKey}`)
                     }
                   }
+                  if (beforeData == null) {
+                    this.mainLogger.error(`repairToMatchReceipt: statsDataSummaryUpdate2 beforeData data is null ${utils.stringifyReduce(data.accountId)} WILL CAUSE DATA OOS  tx:${shortHash}  acc:${shortKey}`)
+                  } else {
+                    if (this.stateManager.partitionStats.hasAccountBeenSeenByStats(data.accountId) === false) {
+                      // Init stats because we have not seen this account yet.
+                      this.stateManager.partitionStats.statsDataSummaryInitRaw(queueEntry.cycleToRecordOn, data.accountId, beforeData)
+                    }
 
-                  let test4 = false
-                  if(isGlobal === false){
-                    let hash = this.stateManager.accountCache.getAccountHash(data.accountId)
-                    test4 = hash.h === updatedHash
+                    // important to update the timestamp.  There are various reasons it could be incorrectly set to 0
+                    let { timestamp: updatedTimestamp, hash: updatedHash } = this.app.getTimestampAndHashFromAccount(data.data)
+                    if (data.timestamp != updatedTimestamp) {
+                      this.mainLogger.error(`repairToMatchReceipt: statsDataSummaryUpdate2 timstamp had to be corrected from ${data.timestamp} to ${updatedTimestamp}   tx:${shortHash}  acc:${shortKey} hash:${utils.stringifyReduce(updatedHash)} `)
+                    }
+                    // This is correcting the timestamp on the wrapper
+                    data.timestamp = updatedTimestamp
+
+                    // update stats
+                    this.stateManager.partitionStats.statsDataSummaryUpdate2(queueEntry.cycleToRecordOn, beforeData, data)
+
+                    // record state table data
+                    let { timestamp: oldtimestamp, hash: oldhash } = this.app.getTimestampAndHashFromAccount(beforeData)
+
+                    let hashNeededUpdate = oldhash !== result.beforeHashes[data.accountId]
+                    oldhash = result.beforeHashes[data.accountId]
+
+                    let stateTableResults: Shardus.StateTableObject = {
+                      accountId: data.accountId,
+                      txId: queueEntry.acceptedTx.id,
+                      stateBefore: oldhash,
+                      stateAfter: updatedHash,
+                      txTimestamp: `${updatedTimestamp}`,
+                    }
+
+                    let updateStateTable = true
+                    let timeStampMatches = updatedTimestamp === queueEntry.acceptedTx.timestamp
+                    let test2 = false
+                    if (timeStampMatches === false) {
+                      if (this.stateManager.accountGlobals.isGlobalAccount(data.accountId)) {
+                        updateStateTable = false
+                        test2 = true
+                      }
+                    }
+                    
+                    let isGlobal = this.stateManager.accountGlobals.isGlobalAccount(data.accountId)
+                    let test3 = false
+                    if (isGlobal) {
+                      if (oldhash === updatedHash) {
+                        updateStateTable = false
+                        test3 = true
+                      }
+                    }
+
+                    let test4 = false
+                    if(isGlobal === false){
+                      let hash = this.stateManager.accountCache.getAccountHash(data.accountId)
+                      test4 = hash.h === updatedHash
+                    }
+
+                    if (updateStateTable === true) {
+                      nestedCountersInstance.countEvent('repair1', 'addAccountStates')
+                      await this.storage.addAccountStates(stateTableResults)
+                    }
+                    if (logFlags.debug) this.mainLogger.debug(`repairToMatchReceipt: addAccountStates tx:${shortHash} neededUpdate:${hashNeededUpdate} updateStateTable:${updateStateTable} timeStampMatches:${timeStampMatches} test2:${test2} test3:${test3} test4:${test4} ${utils.stringifyReduce(stateTableResults)}  acc:${shortKey}`)
                   }
 
-                  if (updateStateTable === true) {
-                    await this.storage.addAccountStates(stateTableResults)
-                  }
-                  if (logFlags.debug) this.mainLogger.debug(`repairToMatchReceipt: addAccountStates tx:${shortHash} neededUpdate:${hashNeededUpdate} updateStateTable:${updateStateTable} timeStampMatches:${timeStampMatches} test2:${test2} test3:${test3} test4:${test4} ${utils.stringifyReduce(stateTableResults)}  acc:${shortKey}`)
+                } finally {
+                  this.profiler.profileSectionEnd('repair_saving_account_data')
                 }
               }
 
