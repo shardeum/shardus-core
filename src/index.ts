0241ae0a1b80b3b641dcc6e826fedf8debc73437
Aamir Syed <asyed94@gmail.com>
2021-06-17 01:31:50 -0500

    Changes `shardus-global-server` modules default export to Shardus, ShardusTypes, and shardusFactory

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,20 @@ import { join } from 'path'
 import merge from 'deepmerge'
 import { readJsonDir } from './utils'
 import Shardus from './shardus'
+export { default as Shardus } from './shardus'
+import * as ShardusTypes from './shardus/shardus-types'
+export {ShardusTypes}
+
 const defaultConfigs = {
   server: require('./config/server.json'),
   logs: require('./config/logs.json'),
-  storage: require('./config/storage.json')
+  storage: require('./config/storage.json'),
 }
 
 const overwriteMerge = (target, source, options) => source
 
-function shardusFactory(configs = {}) {
+export function shardusFactory(configs = {}) {
   return new Shardus(
     merge(defaultConfigs, configs, { arrayMerge: overwriteMerge })
   )
 }
-
-export default shardusFactory
