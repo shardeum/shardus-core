46c4c78429e0f15c0199ea71f642ba3eb0b2b6c9
Aamir Syed <asyed94@gmail.com>
2022-02-10 19:40:50 -0600

    Fixes path of imported types in build-index.d.ts

--- scripts/build-index.d.ts
+++ scripts/build-index.d.ts
@@ -1,4 +1,5 @@
-import Shardus from '../build/src/shardus'
-import { ShardusConfiguration } from '../build/src/shardus/shardus-types';
+// @ts-nocheck
+import Shardus from './src/shardus' // Path is correct in resulting dist package
+import { ShardusConfiguration } from './src/shardus/shardus-types' // Path is correct in resulting dist package
 
 declare function shardusFactory(configs?: ShardusConfiguration): Shardus
