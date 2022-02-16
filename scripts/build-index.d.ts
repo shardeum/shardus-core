a442491946193a711ebff44e09560de6ed3c8208
Aaron Sullivan <aasullivan1618@gmail.com>
2022-02-15 19:01:36 -0800

    Update build-index.d.ts in attempt to export types from server-dist
    This is somewhat of a shot in the dark, I want typing to be exported
    from SGS when used as a library by a dapp developer. This is the
    thirdish commit attempting to do that, and we haven't seen success yet.
    However, I believe this could be the one!
    
    Furthermore, in exploring this, I've become more familiar with this
    build process, which I call a victory :)

--- scripts/build-index.d.ts
+++ scripts/build-index.d.ts
@@ -1,5 +1,6 @@
 // @ts-nocheck
 import Shardus from './src/shardus' // Path is correct in resulting dist package
 import { ShardusConfiguration } from './src/shardus/shardus-types' // Path is correct in resulting dist package
+export * from './src/shardus/shardus-types'
 
 declare function shardusFactory(configs?: ShardusConfiguration): Shardus
