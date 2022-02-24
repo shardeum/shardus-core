0b2aa19d723fb35193377b5c86e3294bc2ce375e
Kaung Myat Thu <2962000kaungmyatthu@gmail.com>
2022-02-24 21:11:28 +0630

    feat: shardus will now be forced to fatal upon incorrect config object being passed
    Previously shardus won't not be fatal upon when there's malformed config
    object is being passed but will introduce tricky bugs later down the line
    
    This commit now force shardus upon incorrect config object being fed and
    console log which config setting is the problem

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import merge from 'deepmerge'
 
 import Shardus from './shardus'
 import * as ShardusTypes from './shardus/shardus-types'
-import { compareObjShape } from './utils'
+import { compareObjectShape } from './utils'
 
 export { default as Shardus } from './shardus'
 export { ShardusTypes }
@@ -20,10 +20,27 @@ export function shardusFactory(configs = {}) {
     arrayMerge: overwriteMerge,
   })
 
-  if (!compareObjShape(defaultConfigs, mergedConfigs)) {
-    throw Error(
-      'Fatal: malformed config settings, unacceptable config object !!'
+  const { isValid, error } = compareObjectShape(defaultConfigs, mergedConfigs)
+
+  if (error) {
+    const fRed = '\x1b[31m' //fg red
+    const bYellow = '\x1b[43m' //bg yellow
+    const defectiveObjectPath = `${fRed}${bYellow}${error.defectiveChain.join('.')}\x1b[0m`
+    const msg = `Unacceptable config object shape, defective settings detected: ${defectiveObjectPath}`
+
+    console.log(
+      '\x1b[1m', //bold, bright
+      '\x1b[31m', //fg red
+      'INVALID CONFIG OBJECT PROPERTY OR TYPE MISMATCH OCCURS:',
+      `${defectiveObjectPath}`
     )
+    console.log(
+      '\x1b[36m', //cyan
+      'For more information on configuration object, check the documentation',
+      '\x1b[0m' //color reset
+    )
+
+    throw new Error(msg)
   }
 
   return new Shardus(mergedConfigs)
