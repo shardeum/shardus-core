9fdb51a2da80ba3f5297ac38c77bf6285a136b04
Thant Sin Toe <mr.thantsintoe@gmail.com>
2022-04-20 23:09:40 +0630

    Minor update to dev signature checks on the debug endpoints

--- src/network/debugMiddleware.ts
+++ src/network/debugMiddleware.ts
@@ -1,6 +1,7 @@
 import { isDebugMode, getHashedDevKey, getDevPublicKey } from '../debug'
 
 import * as Context from '../p2p/Context'
+import { signObj } from '@shardus/crypto-utils'
 
 let lastCounter = 0
 
@@ -8,18 +9,16 @@ export const isDebugModeMiddleware = (_req, res, next) => {
   const isDebug = isDebugMode()
 
   if (!isDebug) {
-
-
-    try{
+    try {
       //auth with by checking a password against a hash
-      if(_req.query.auth != null){
-        const hashedAuth = Context.crypto.hash({key:_req.query.auth})
+      if (_req.query.auth != null) {
+        const hashedAuth = Context.crypto.hash({ key: _req.query.auth })
         const hashedDevKey = getHashedDevKey()
         // can get a hash back if no key is set
-        if(hashedDevKey === ""){
-          return res.json({hashedAuth})
+        if (hashedDevKey === '') {
+          return res.json({ hashedAuth })
         }
-        if(hashedAuth === hashedDevKey){
+        if (hashedAuth === hashedDevKey) {
           next()
           return
         }
@@ -29,17 +28,25 @@ export const isDebugModeMiddleware = (_req, res, next) => {
         const ownerPk = getDevPublicKey()
         let requestSig = _req.query.sig
         //check if counter is valid
-        let sigObj = {route: _req.route, count:_req.query.sig_counter, sign: {owner:ownerPk,sig:requestSig } }
-        
+        let sigObj = {route: _req.route.path, count: String(_req.query.sig_counter), sign: {owner:ownerPk,sig:requestSig } }
+
         //reguire a larger counter than before.
-        if(sigObj.count > lastCounter){
+        if(parseInt(sigObj.count) > lastCounter){
           let verified = Context.crypto.verify(sigObj,  ownerPk)
           if(verified === true){
-            //update counter so we can't use it again 
-            lastCounter = sigObj.count
+            //update counter so we can't use it again
+            lastCounter = parseInt(sigObj.count)
             next()
             return
+          } else {
+            console.log('Signature is not correct', sigObj, lastCounter)
           }
+        } else {
+          console.log(
+            'Counter is not larger than last counter',
+            sigObj.count,
+            lastCounter
+          )
         }
       }
     } catch(error){
