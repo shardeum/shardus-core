24eb0f7de35eca30d9a21de17dc7368e638710d3
Andrew Foster <3023784-afost@users.noreply.gitlab.com>
2022-08-04 21:52:46 -0500

    revert cycle fix to see if it helps

--- src/p2p/CycleCreator.ts
+++ src/p2p/CycleCreator.ts
@@ -517,7 +517,9 @@ function makeCycleRecord(
   prevRecord?: P2P.CycleCreatorTypes.CycleRecord
 ): P2P.CycleCreatorTypes.CycleRecord {
   const baseRecord: P2P.CycleCreatorTypes.BaseRecord = {
-    networkId: prevRecord ? prevRecord.networkId : randomBytes(32),
+    //networkId: prevRecord ? prevRecord.networkId : randomBytes(32),
+    //reverted fix to this old version to see if that fixes cycle sync
+    networkId: crypto.hash({ rand: Math.floor(Math.random() * 1000000) }),
     counter: prevRecord ? prevRecord.counter + 1 : 0,
     previous: prevRecord ? makeCycleMarker(prevRecord) : '0'.repeat(64),
     start: prevRecord
