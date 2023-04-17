97728c24895acf2b4fff5b9214fb155bf360808e
Andrew Foster <3023784-afost@users.noreply.gitlab.com>
2023-04-17 02:26:00 -0500

    missed this

--- src/config/server.ts
+++ src/config/server.ts
@@ -151,7 +151,7 @@ const SERVER_CONFIG: StrictServerConfiguration = {
     apopFromStuckProcessing: false,
     discardVeryOldPendingTX: false,
     transactionApplyTimeout: -1, //ms for timeout. something like 7000 is a starting point. todo set to -1 before release
-    fifoUnlockFix: true, //todo for production set this to false.
+    fifoUnlockFix: false, //todo for production set this to false.
     //disableSyncFifoClear:false //avoid clearing fifo locks when sync bails?
     fifoUnlockFix2: false,
     fifoUnlockFix3: false,
