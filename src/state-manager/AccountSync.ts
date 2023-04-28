77ca0ce0863aad112da65eae939821c6168f11c4
Andrew Foster <3023784-afost@users.noreply.gitlab.com>
2023-04-28 00:15:19 -0500

    a guess at a fix

--- src/state-manager/AccountSync.ts
+++ src/state-manager/AccountSync.ts
@@ -939,9 +939,11 @@ class AccountSync {
       let result = await this.p2p.ask(node, 'get_globalaccountreport', {})
       if (result === false) {
         /* prettier-ignore */ if (logFlags.error) this.mainLogger.error(`ASK FAIL getRobustGlobalReport result === false node:${utils.stringifyReduce(node.id)}`)
+        result = { ready: false, msg: `result === false: ${Math.random()}` }
       }
       if (result === null) {
         /* prettier-ignore */ if (logFlags.error) this.mainLogger.error(`ASK FAIL getRobustGlobalReport result === null node:${utils.stringifyReduce(node.id)}`)
+        result = { ready: false, msg: `result === null: ${Math.random()}` }
       }
 
       // TODO I dont know the best way to handle a non null network error here, below is something I had before but disabled for some reason
