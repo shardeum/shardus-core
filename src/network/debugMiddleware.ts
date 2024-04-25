d2748dabad4ff9029a7d4bc4abca931fc93710a6
urnotsam <samwsweet@gmail.com>
2024-04-25 07:54:28 -0400

    Revert "Add stringify util to sanitize objects to prevent log injection attacks"
    This reverts commit 6189e314520f507005224aa5d90e19a17a592ff6.

--- src/network/debugMiddleware.ts
+++ src/network/debugMiddleware.ts
@@ -2,7 +2,6 @@ import { isDebugMode, getHashedDevKey, getDevPublicKeys, ensureKeySecurity } fro
 import * as Context from '../p2p/Context'
 import * as crypto from '@shardus/crypto-utils'
 import { DevSecurityLevel } from '../shardus/shardus-types'
-import { escapeLogStringify } from '../utils'
 
 const MAX_COUNTER_BUFFER_MILLISECONDS = 10000
 let lastCounter = 0
@@ -39,14 +38,13 @@ function handleDebugAuth(_req, res, next, authLevel) {
               })
             }
           } else {
-            const sanitizedSigObject = escapeLogStringify(sigObj)
-            console.log('Signature is not correct', sanitizedSigObject)
+            console.log('Signature is not correct', sigObj, currentCounter)
           }
         } else {
-          const sanitizedSigObject = escapeLogStringify(sigObj)
           console.log(
-            'Counter is more than 10 seconds old or less than last counter. SigObj: ',
-            sanitizedSigObject,
+            'Counter is more than 10 seconds old or less than last counter. Counter: ',
+            sigObj,
+            currentCounter,
             'last counter:',
             lastCounter
           )
@@ -162,24 +160,3 @@ export const isDebugModeMiddlewareMultiSig = (_req, res, next) => {
     handleMultiDebugAuth(_req, res, next)
   } else next()
 }
-
-function escapeLogValue(value) {
-  if (typeof value === 'string') {
-    return value.replace(/[\r\n\t%]/g, (match) => {
-      switch (match) {
-        case '\r':
-          return '\\r'
-        case '\n':
-          return '\\n'
-        case '\t':
-          return '\\t'
-        case '%':
-          return '%25'
-        default:
-          return match
-      }
-    })
-  }
-  return value
-}
-
