654383acf40e10197ccf7ef52521d48e0515a16f
Arham Jain <arham.nec@gmail.com>
2024-04-29 14:43:43 +0530

    feat(api): added error handling for binary_get_cached_app_data

--- src/state-manager/CachedAppDataManager.ts
+++ src/state-manager/CachedAppDataManager.ts
@@ -585,18 +585,24 @@ class CachedAppDataManager {
 
       const message = { topic, dataId }
       let r: CachedAppData | boolean
-      if (this.config.p2p.useBinarySerializedEndpoints && this.config.p2p.getCachedAppDataBinary) {
-        const resp = await this.p2p.askBinary<GetCachedAppDataReq, GetCachedAppDataResp>(
-          randomConsensusNode,
-          InternalRouteEnum.binary_get_cached_app_data,
-          message,
-          serializeGetCachedAppDataReq,
-          deserializeGetCachedAppDataResp,
-          {}
-        )
-        r = resp?.cachedAppData
-      } else {
-        r = await this.p2p.ask(randomConsensusNode, 'get_cached_app_data', message)
+      try {
+        if (this.config.p2p.useBinarySerializedEndpoints && this.config.p2p.getCachedAppDataBinary) {
+          const resp = await this.p2p.askBinary<GetCachedAppDataReq, GetCachedAppDataResp>(
+            randomConsensusNode,
+            InternalRouteEnum.binary_get_cached_app_data,
+            message,
+            serializeGetCachedAppDataReq,
+            deserializeGetCachedAppDataResp,
+            {}
+          )
+          r = resp?.cachedAppData
+        } else {
+          r = await this.p2p.ask(randomConsensusNode, 'get_cached_app_data', message)
+        }
+      } catch (e) {
+        this.mainLogger.error(`cachedAppData: ASK FAIL getLocalOrRemoteCachedAppData`, e)
+        nestedCountersInstance.countEvent('cached-app-data', 'ask failed')
+        r = false
       }
 
       if (r === false) {
