b3db9f058914cc3fbfc5471684f18f4cf1ed8e3b
Ahmed <iaahmed40@gmail.com>
2024-04-28 23:06:35 -0500

    more debug logs

--- src/p2p/Join/routes.ts
+++ src/p2p/Join/routes.ts
@@ -490,7 +490,7 @@ const gossipSyncFinishedRoute: P2P.P2PTypes.GossipHandler<P2P.JoinTypes.Finished
   if (config.debug.cycleRecordOOSDebugLogs) {
     if (receivedSyncFinished.has(payload.nodeId) === false) {
       receivedSyncFinished.set(payload.nodeId, true)
-      console.log('DEBUG CR-OOS: payload id: ', payload.nodeId)
+      console.log('DEBUG CR-OOS: payload id: ', payload.nodeId, ' payload cycle: ', payload.cycleNumber, 'cC: ', CycleCreator.currentCycle, ' cQ: ', CycleCreator.currentQuarter)
     }
   }
 
@@ -508,7 +508,12 @@ const gossipSyncFinishedRoute: P2P.P2PTypes.GossipHandler<P2P.JoinTypes.Finished
     // Validate payload in addFinishedSyncing
     const addFinishedSyncingResult = addFinishedSyncing(payload)
     nestedCountersInstance.countEvent('p2p', `sync-finished validation success: ${addFinishedSyncingResult.success}`)
-    if (!addFinishedSyncingResult.success) nestedCountersInstance.countEvent('p2p', `sync-finished failure reason: ${addFinishedSyncingResult.reason}`)
+    if (!addFinishedSyncingResult.success) {
+      if (addFinishedSyncingResult.reason !== 'node has already submitted syncFinished request') {
+        if (config.debug.cycleRecordOOSDebugLogs) console.log('DEBUG CR-OOS: gossipSyncFinished rejected: ', addFinishedSyncingResult.reason, ' payload id: ', payload.nodeId, ' payload cycle: ', payload.cycleNumber)
+      }
+      nestedCountersInstance.countEvent('p2p', `sync-finished failure reason: ${addFinishedSyncingResult.reason}`)
+    }
     if (addFinishedSyncingResult.success) {
       Comms.sendGossip('gossip-sync-finished', payload, tracker, sender, NodeList.byIdOrder, false)
     } else {
