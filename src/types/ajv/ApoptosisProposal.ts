0138a94ba211fab744fd54dabce4deb8755b7752
Sonali Thakur <sonalithakur1199@gmail.com>
2024-05-06 16:49:51 +0530

    Data validation test for ApoptosisProposalResp

---
+++ src/types/ajv/ApoptosisProposal.ts
@@ -0,0 +1,25 @@
+import { addSchema } from '../../utils/serialization/SchemaHelpers'
+
+const schemaApoptosis = {
+  type: 'object',
+  properties: {
+    s: { type: 'string' },
+    r: { type: 'number' },
+  },
+  required: ['s', 'r'],
+}
+
+export function initApoptosisProposal(): void {
+  addSchemaDependencies()
+  addSchemas()
+}
+
+// Function to add schema dependencies
+function addSchemaDependencies(): void {
+  // No dependencies
+}
+
+// Function to register the schema
+function addSchemas(): void {
+  addSchema('ApoptosisProposal', schemaApoptosis)
+}
