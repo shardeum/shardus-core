7800fab1e1916fee69e2e006bf4d69ddcfd0c227
Ahmed <iaahmed40@gmail.com>
2024-05-13 08:11:03 -0500

    debug logs to track status of nodes

--- src/p2p/Comms.ts
+++ src/p2p/Comms.ts
@@ -693,6 +693,8 @@ export function isNodeValidForInternalMessage(
 
   // Also may add a flag to change if we also allow other statuses
   if (nodeStatus != 'active' || NodeList.potentiallyRemoved.has(node.id)) {
+    if (config.debug.cycleRecordOOSDebugLogs && nodeStatus != 'active') console.log(`DEBUG CR-OOS: isNodeValidForInternalMessage: filtering ${node.externalPort} b/c its not active. Node object:`, node)
+    if (config.debug.cycleRecordOOSDebugLogs && NodeList.potentiallyRemoved.has(node.id)) console.log(`DEBUG CR-OOS: isNodeValidForInternalMessage: filtering ${node.externalPort} b/c its potentially removed`)
     if (logErrors)
       if (logFlags.error)
         /* prettier-ignore */ error(`isNodeValidForInternalMessage node not active. ${nodeStatus} ${utils.stringifyReduce(node.id)} ${debugMsg}`)
