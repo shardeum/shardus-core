1f45d9806b9fa8159b2f7d233511eab8d6823775
urnotsam <samwsweet@gmail.com>
2024-05-16 14:12:19 -0400

    add nodeListFromStates for remaining cycle gossip

--- src/p2p/CycleCreator.ts
+++ src/p2p/CycleCreator.ts
@@ -46,6 +46,7 @@ import path from 'path'
 import { getStreamWithTypeCheck, requestErrorHandler } from '../types/Helpers'
 import { RequestErrorEnum } from '../types/enum/RequestErrorEnum'
 import { BadRequest, InternalError, NotFound, serializeResponseError } from '../types/ResponseError'
+import { nodeListFromStates } from './Join'
 
 /** CONSTANTS */
 
@@ -1281,7 +1282,14 @@ async function gossipCycleCert(sender: P2P.NodeListTypes.Node['id'], tracker?: s
     record: bestRecord,
   }
   const signedCertGossip = crypto.sign(certGossip)
-  Comms.sendGossip('gossip-cert', signedCertGossip, tracker, sender, NodeList.byIdOrder, true)
+  Comms.sendGossip(
+    'gossip-cert',
+    signedCertGossip,
+    tracker,
+    sender,
+    nodeListFromStates(['active', 'ready', 'syncing']),
+    true
+  )
 }
 
 function pruneCycleChain() {
