ff79f5412391e1930d953821a6991c0afbaf1cce
jairajdev <jairajranamagar.dev@gmail.com>
2024-06-03 23:14:36 +0545

    Updated to gossip all status nodes during restart network

--- src/p2p/Join/index.ts
+++ src/p2p/Join/index.ts
@@ -1541,6 +1541,7 @@ function decideNodeSelection(joinRequest: P2P.JoinTypes.JoinRequest): JoinReques
 }
 
 export function nodeListFromStates(states: P2P.P2PTypes.NodeStatus[]): P2P.NodeListTypes.Node[] {
+  if (Self.isRestartNetwork) return NodeList.byIdOrder
   const { NodeStatus } = P2P.P2PTypes
   const stateMappings: { [key in P2P.P2PTypes.NodeStatus]?: P2P.NodeListTypes.Node[] } = {
     [NodeStatus.ACTIVE]: NodeList.activeByIdOrder,
