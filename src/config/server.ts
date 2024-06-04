fdeb18127f448e03d59923c3ec6bd1e21959316f
Thant Sin Toe <mr.thantsintoe@gmail.com>
2024-06-04 20:52:30 +0700

    initial implementation of factTellCorrespondingNodes and factValidateCorrespondingTellSender functions

--- src/config/server.ts
+++ src/config/server.ts
@@ -180,6 +180,7 @@ const SERVER_CONFIG: StrictServerConfiguration = {
     downNodeFilteringEnabled: false,
     useFactCorrespondingTell: true,
     resubmitStandbyAddWaitDuration: 1000, // 1 second in ms
+    useFactCorrespondingTell: true
   },
   ip: {
     externalIp: '0.0.0.0',
