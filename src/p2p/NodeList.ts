67260e9654816fda8ccd84c2e1c8979852e9b47e
urnotsam <samwsweet@gmail.com>
2024-08-01 11:49:40 -0400

    add activeCycle when node deactivates

--- src/p2p/NodeList.ts
+++ src/p2p/NodeList.ts
@@ -268,15 +268,17 @@ export function removeNode(
         cycleNumber: cycle.counter,
       }
       emitter.emit('node-left-early', emitParams)
+    } else {
+      const emitParams: Omit<ShardusEvent, 'type'> = {
+        nodeId: node.id,
+        reason: 'Node deactivated',
+        time: cycle.start,
+        publicKey: node.publicKey,
+        cycleNumber: cycle.counter,
+        activeCycle: node.activeCycle,
+      }
+      emitter.emit('node-deactivated', emitParams)
     }
-    const emitParams: Omit<ShardusEvent, 'type'> = {
-      nodeId: node.id,
-      reason: 'Node deactivated',
-      time: cycle.start,
-      publicKey: node.publicKey,
-      cycleNumber: cycle.counter,
-    }
-    emitter.emit('node-deactivated', emitParams)
   }
 }
 
